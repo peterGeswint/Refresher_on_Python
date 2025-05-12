3a505396083b940d2b92eda3b33afe1bd58c1349
Peter Clifton Geswint <115699568+peterGeswint@users.noreply.github.com>
2025-05-12 22:00:14 +0200

    More information on functions

--- Functions-in-python.py
+++ Functions-in-python.py
@@ -27,3 +27,21 @@ def square(x):
 	return x * x
 
 square(5)
+
+#lambda expression
+add1 = lambda x,y: x + y
+num = add1(5,6)
+print(num)
+
+# lambda expression
+# to convert celsius to fahrenheit
+c= [30,23,44,55,66,77,88,99]
+f = list(map(lambda x: (float(9)/5)*(x + 32), c))
+print(f)
+
+# a funtion that returns a int and string.
+def printInfo(nameandsurname, age):
+	print("Name and Surname: ", nameandsurname)
+	print("Age: ", age)
+	return
+printInfo("John Doe", 30)
